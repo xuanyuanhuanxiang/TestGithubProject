12a2c97772692f4204b0a70186f9731d72043319
xuanyuanhuanxiang <873466612@qq.com>
2018-11-29 09:32:51 +0800

    Initial commit

---
+++ src/main/java/com/aaa/testgh/controller/DeptController.java
@@ -0,0 +1,15 @@
+package com.aaa.testgh.controller;
+
+/**
+ * className:DeptController
+ * discription:
+ * author:zz
+ * createTime:2018-11-29 09:07
+ */
+public class DeptController {
+
+    public static void main(String[] args) {
+        System.out.println("测试github提交。。。。");
+    }
+
+}
